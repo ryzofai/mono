31350ec0826b89c810775f28b0f7607ee24a787a
ryzofai <38214893+ryzofai@users.noreply.github.com>
2018-04-12 16:23:47 +0800

    Update twt.py

--- pys/twt.py
+++ pys/twt.py
@@ -1,12 +1,12 @@
 import twitter
 
-api = twitter.Api(consumer_key='Ni9rZZFOiAHPgnalGxauIReov',
-                  consumer_secret='Om3d24bW7NYF3Xst5NxzOXNy51jAV7QUUTB1twYPMWfpLYCU5i',
-                  access_token_key='984061142724063233-lvDldadmfua6h7yWHDjNYNb55v1oT5A',
-                  access_token_secret='Xz3n1fAw2A5Dn9lapb68L1f2ORL0tussncqT3Eg98oQPK')
+api = twitter.Api(consumer_key='',
+                  consumer_secret='',
+                  access_token_key='',
+                  access_token_secret='')
 
 try:
-    status = api.PostUpdate('Hello from LBP.SRE Twitter app! \nThis app is still in Beta.')
+    status = api.PostUpdate('Hello from SRE Twitter app! \nThis app is still in Beta.')
 except UnicodeDecodeError:
     print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
     print("Try explicitly specifying the encoding with the --encoding flag")
